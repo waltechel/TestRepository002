9aac0ce59642541f2589d67e04f7a17aff10a42f
knecht <ldj910505@naver.com>
2021-05-27 22:43:32 +0900

    <code>(일반) 내용을 추가
    반복적 내용 추가

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,14 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out.println("원복했습니다.");
+
+		System.out.println("이동준이 짜서 올린 부분");
+
+		int answer = 0;
+		for (int i = 0; i < 10; i++) {
+			answer += i;
+		}
+		System.out.println(answer);
 	}
 
 }
